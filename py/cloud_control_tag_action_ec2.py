46bb7f332d0e18fd4f1a62fcb9ca3fe81ba65a42
Pawel Piwosz <pawel@pawelpiwosz.net>
2019-08-04 00:04:51 +0200

    rebuild in progress

--- py/cloud_control_tag_action_ec2.py
+++ py/cloud_control_tag_action_ec2.py
@@ -4,38 +4,57 @@ import boto3
 def cloud_control_state_action_ec2(event, context):
     """ Lambda function - add/change/remove tag for ec2 """
 
-
-
-
-# ------- To refactor
-
-def ec_tags_describe(instance_id, tag_name, tag_value):
-    """Check if Tag exists with given value"""
+    # validate instance name
+    ec2 = boto3.resource('ec2')
     ec2_client = boto3.client('ec2')
-    response = ec2_client.describe_tags(
+    response = ec2_client.describe_instances(
+        Filters=[
+            {
+                'Name': 'tag:Name',
+                'Values': [event["body"]["InstanceName"]]
+            }
+        ]
+    )
+    instance_list = []
+    for reservation in response['Reservations']:
+        for instance in reservation['Instances']:
+            instance_list.append(instance['InstanceId'])
+
+    if not instance_list:
+        msg = "I cannot find the instance with name {}.".format(event["body"]["InstanceName"])
+        return {"msg": msg}
+
+    ec2_instance = ec2.instances.filter(InstanceIds=instance_list)
+
+    # validate tag
+    tag_response = ec2_client.describe_tags(
         Filters=[
             {
                 'Name': 'resource-id',
                 'Values': [
-                    instance_id,
+                    ec2_instance,
                 ],
             },
         ],
     )
-    for tag in response['Tags']:
-        if tag['Key'] == tag_name:
-            if tag['Value'] == tag_value:
+    tag_status = "not_found" #2
+    for tag in tag_response['Tags']:
+        if tag['Key'] == event["body"]["TagKey"]:
+            if tag['Value'] == event["body"]["TagValue"]:
                 msg = "Tag {} with value {} found.".format(
-                    tag_name, tag_value
+                    event["body"]["TagKey"], event["body"]["TagValue"]
                 )
-                return 0, msg
+                tag_status = "tag_match" #0
             else:
                 msg = "tag {} found, but value is different".format(
-                    tag_name
+                    event["body"]["TagKey"]
                 )
-                return 1, msg
-    msg = "Tag {} not found!".format(tag_name)
-    return 2, msg
+                tag_status = "tag_different" #1
+        msg = "Tag {} not found!".format(event["body"]["TagKey"])
+
+
+
+# ------- To refactor
 
 def ec_tag_action(ec_params):
     """Add, remove, update Ec2 tag"""
